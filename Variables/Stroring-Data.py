28d12f79c5942206d9863c1a2e018df87c3942ab
MuhammadAbuBakarQureshi <qureshimuhammadabubakar@gmail.com>
2024-06-17 22:07:30 +0500

    Storing-Data.py file updated

--- Variables/Stroring-Data.py
+++ Variables/Stroring-Data.py
@@ -1,3 +1,5 @@
+# Python is case sensitive
+
 name = "Muhammad Abu Bakar"
 
 age = 18
@@ -12,6 +14,9 @@ name = "Muhammad"
 
 age = 19
 
+is_adult = True # true with smaller "t" gives error beacuse python is case sensitive
+
 print("My name is", name)
 
 print("I am", age, "year old")
+
