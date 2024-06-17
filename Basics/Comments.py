f58f28a6d8b8929d78b89d00e73f888f3b30563c
MuhammadAbuBakarQureshi <qureshimuhammadabubakar@gmail.com>
2024-06-17 22:03:22 +0500

    Comments.py file added

---
+++ Basics/Comments.py
@@ -0,0 +1,21 @@
+
+# Single line comments
+
+# This is single line comment
+
+
+
+#Multi-Line Comments
+
+# You can also write
+# multi-line comments in
+# this way by including
+# the comment character 
+# at the beginning of every line
+
+
+'''
+This is multi-line comment
+with the use of 
+multi-line string
+'''
