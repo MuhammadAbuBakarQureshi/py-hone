4ef51d0d5ac6fbb17b9055c68a0e61d3d4555fbe
MuhammadAbuBakarQureshi <qureshimuhammadabubakar@gmail.com>
2024-06-17 21:56:19 +0500

    Added two folders

---
+++ Variables/Stroring-Data.py
@@ -0,0 +1,17 @@
+name = "Muhammad Abu Bakar"
+
+age = 18
+
+print("My name is", name)
+
+print("I am", age, "year old")
+
+#You can also change the value of the variable
+
+name = "Muhammad"
+
+age = 19
+
+print("My name is", name)
+
+print("I am", age, "year old")
