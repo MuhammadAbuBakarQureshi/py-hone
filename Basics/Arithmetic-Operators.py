244e8b14454deb4c2fc5e87b87603722c903f30f
MuhammadAbuBakarQureshi <qureshimuhammadabubakar@gmail.com>
2024-06-18 20:37:34 +0500

    Arithmetic-Operators file added

---
+++ Basics/Arithmetic-Operators.py
@@ -0,0 +1,29 @@
+# Addition operator
+
+print(5 + 3) # 8
+
+# Subtraction operator
+
+print(10 - 5) # 5
+
+# Multiplication operator
+
+print(5 * 4) # 20
+
+# Division operator
+
+print(5 / 2) # 2.5
+
+# If we use "//" then numbers after decimal will not be printed
+
+print(5 // 2) # 2
+
+# Modulo operator
+# It gives the remainder
+
+print(5 % 2) # 1
+
+
+# Power operator
+
+print( 5 ** 2) # 25
