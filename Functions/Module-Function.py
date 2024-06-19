d90f90b661853a65fc98c4695801f1047f2907d1
MuhammadAbuBakarQureshi <qureshimuhammadabubakar@gmail.com>
2024-06-19 14:16:11 +0500

    Module-Function file added

---
+++ Functions/Module-Function.py
@@ -0,0 +1,28 @@
+'''
+Importing math module
+
+import math
+
+'''
+
+# You can import just one function from module
+
+from math import sqrt
+
+
+num = input("Enter the number : ")
+
+num = int(num)
+
+
+'''
+using function from module
+
+print(math.sqrt(num))
+
+'''
+
+
+# Using function When you have just imported that function only
+
+print(sqrt(num))
