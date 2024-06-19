03b4a81fd024d02a119d3bbc2237fdba5e6c7820
MuhammadAbuBakarQureshi <qureshimuhammadabubakar@gmail.com>
2024-06-19 14:20:40 +0500

    Functions folder added

--- Functions/Module-Function.py
+++ Functions/Module-Function.py
@@ -1,13 +1,28 @@
-'''
-Importing math module
+# Importing math module
 
 import math
 
+
+# To check all function from the module
+
+print(dir(math))
+
+'''
+To import all Functions from the module we use asterisk
+
+from math import *
 '''
 
-# You can import just one function from module
+
+
+
+'''
+You can import just one function from module
 
 from math import sqrt
+
+'''
+
 
 
 num = input("Enter the number : ")
@@ -15,14 +30,12 @@ num = input("Enter the number : ")
 num = int(num)
 
 
-'''
-using function from module
+# using function from module
 
 print(math.sqrt(num))
 
-'''
 
 
 # Using function When you have just imported that function only
 
-print(sqrt(num))
+# print(sqrt(num))
