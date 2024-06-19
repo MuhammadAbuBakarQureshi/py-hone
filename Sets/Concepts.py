3a7ee5d1010d50208c896eaac561c6c105be3768
MuhammadAbuBakarQureshi <qureshimuhammadabubakar@gmail.com>
2024-06-19 12:36:24 +0500

    Sets Folder added

---
+++ Sets/Concepts.py
@@ -0,0 +1,21 @@
+'''
+It is a collection that is written with curly brackets and is both unindexed and unordered	
+
+Items in the sets are Imputable
+
+Items in the sets are Unique.
+
+You cannot use index to print value from the SETS
+
+You can use loop to iterate in the SETS.
+'''
+
+marks = {60, 83, 72, 94}
+
+print(marks)
+
+
+
+for score in marks:
+
+    print(score)
