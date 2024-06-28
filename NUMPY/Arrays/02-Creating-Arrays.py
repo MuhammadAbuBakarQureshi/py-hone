6c8e589b6ba7184c0e3839cc1e4654685c7190e6
MuhammadAbuBakarQureshi <qureshimuhammadabubakar@gmail.com>
2024-06-28 08:39:37 +0500

    02-Creating-Arrays file added in arrays folder

---
+++ NUMPY/Arrays/02-Creating-Arrays.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+'''     The array object in numpy is calles ndarray
+'''
+
+# Creating array
+
+arr = np.array([1, 2, 3])
+
+print(arr)
+
+print(type(arr))
+
+
+
+# Using tuple to create a numpy array
+
+tuple = (2, 4, 6)
+
+tuple_arr = np.array(tuple)
+
+print(tuple_arr)
