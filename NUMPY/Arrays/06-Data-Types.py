a105a8d53a3ce75d7bc725b5e3c55d2dbe280bd6
MuhammadAbuBakarQureshi <qureshimuhammadabubakar@gmail.com>
2024-06-29 08:07:07 +0500

    06-Data-Types file modified

--- NUMPY/Arrays/06-Data-Types.py
+++ NUMPY/Arrays/06-Data-Types.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 '''     Finding Data types
 '''
 
@@ -12,6 +13,31 @@ arr = np.array(["Apple", "Banana"])
 print(arr.dtype)
 
 
+
+
 '''     Creating Arrays With a Defined Data Type
 '''
 
+arr = np.array([4, 2, 6], dtype = 'int16')
+
+print(arr.dtype) # int16
+
+
+
+'''     Get size
+'''
+
+arr = np.array([1 ,3 , 5])
+
+''' "itemsize" gives Each Element size in the array
+'''
+print(arr.itemsize) # 4
+
+
+''' "size" gives How many elements in the array
+'''
+print(arr.size) # 3
+
+
+'''     Float is 8 bytes
+'''
