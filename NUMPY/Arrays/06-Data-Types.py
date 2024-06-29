7e7802562b784de2ddd8b57c20feb478d853a868
MuhammadAbuBakarQureshi <qureshimuhammadabubakar@gmail.com>
2024-06-29 09:04:54 +0500

    06-Data-Types file modified

--- NUMPY/Arrays/06-Data-Types.py
+++ NUMPY/Arrays/06-Data-Types.py
@@ -48,3 +48,34 @@ print(arr.itemsize) # 4
 Float is 8 bytes
 '''
 print(arr.size) # 3
+
+
+
+
+
+
+'''
+                            Converting Data Type on Existing Arrays
+
+                            
+The best way to change the data type of an existing array, is to make a copy of the array with the astype() method.
+
+The astype() function creates a copy of the array, and allows you to specify the data type as a parameter.
+
+The data type can be specified using a string, like 'f' for float, 'i' for integer etc. or you can use the data type directly like float for 
+float and int for integer.
+
+'''
+
+
+arr = np.array([5.2, 7.7, 3.5, 7.1, 4.3 , 6.7])
+
+print(arr.dtype) # float64
+
+new_arr = arr.astype(int)
+
+print(new_arr.dtype) # int32
+
+new_arr = new_arr.astype(bool)
+
+print(new_arr.dtype) # bool
