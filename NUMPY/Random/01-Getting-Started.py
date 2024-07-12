cbfa1599defbd20215f25ec6d2a1854021f86592
MuhammadAbuBakarQureshi <qureshimuhammadabubakar@gmail.com>
2024-07-12 21:36:45 +0500

    01-Getting-Started file added in Random folder

---
+++ NUMPY/Random/01-Getting-Started.py
@@ -0,0 +1,16 @@
+from numpy import random
+
+# Random module used to work with random numbers
+
+
+x = random.randint(100)
+
+print("\nRandom number using randit : ", x)
+
+
+
+# rand() method returns float number between 0 and 1
+
+x = random.rand(2) # if I give 2 as argument here it will return 2 random numbers between 0 and 1 
+
+print("\n\nRandom number using rand : ", x)
